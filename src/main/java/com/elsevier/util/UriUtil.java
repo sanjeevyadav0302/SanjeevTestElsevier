04dcb8add79df0af30ded13e151f46b053a67bbe
sanjeevyadav0302 <sanjeev.yadav3290@gmail.com>
2018-10-23 23:41:30 +0200

    Refactored UriUtil class to use constants

--- src/main/java/com/elsevier/util/UriUtil.java
+++ src/main/java/com/elsevier/util/UriUtil.java
@@ -2,34 +2,45 @@ package com.elsevier.util;
 
 public class UriUtil {
 
-    /** Get public repository Uri
+    private static final String USERS = "users";
+    private static final String REPOS = "repos";
+    private static final String COLLABORATORS = "collaborators";
+    private static final String CONTRIBUTORS = "contributors";
+    private static final String SLASH = "/";
+
+
+    /**
+     * Get public repository Uri
+     *
      * @param userName
      * @return String
      */
     public static String getRepositoryUri(String userName) {
-        String url = "/users/" + userName + "/repos";
-        return url;
+        String repositoryUri = SLASH + USERS + SLASH + userName + SLASH + REPOS;
+        return repositoryUri;
     }
 
     /**
      * Return collaborators Uri
+     *
      * @param owner
      * @param repoName
      * @return String
      */
     public static String getCollaboratorUri(String owner, String repoName) {
-        String url = "/repos/" + owner + "/" + repoName + "/collaborators";
-        return url;
+        String collaboratorsUri = SLASH + REPOS + SLASH + owner + SLASH + repoName + SLASH + COLLABORATORS;
+        return collaboratorsUri;
     }
 
     /**
      * Return Contributors Uri
+     *
      * @param owner
      * @param repoName
      * @return String
      */
     public static String getContributorsUri(String owner, String repoName) {
-        String url = "/repos/" + owner + "/" + repoName + "/contributors";
-        return url;
+        String contributorsUri = SLASH + REPOS + SLASH + owner + SLASH + repoName + SLASH + CONTRIBUTORS;
+        return contributorsUri;
     }
 }
